b771de85f006eb0e6953980411f5a53231ffeb6d
Fidan Babayeva <fidanbabayeva51@gmail.com>
2023-09-30 22:57:13 +0400

    jacoco added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("jacoco")
     id("org.sonarqube") version "4.3.1.3277"
 }
 
@@ -24,5 +25,19 @@ sonar {
         property("sonar.projectKey", "bhos-qa_l4-branch-coverage-Fidan-Babayeva")
         property("sonar.organization", "bhos-qa")
         property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+jacoco {
+    toolVersion = "0.8.9"
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
     }
 }
